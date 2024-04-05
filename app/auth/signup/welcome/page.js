c39b645688265bc0d5735b26ae35a53c69709336
Moein.MAC <moein.mac@gmail.com>
2024-04-05 13:42:43 +0330

    welcome signup almost done

--- app/auth/signup/welcome/page.js
+++ app/auth/signup/welcome/page.js
@@ -2,6 +2,7 @@ import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
 import SignupCircle from "@/components/Auth/SignupCircle";
 import Image from "next/image";
+import Button from "@/components/UI/Button";
 
 const signupWelcome = async () => {
   const supabase = createClient();
@@ -10,13 +11,26 @@ const signupWelcome = async () => {
 
   return (
     <div className="relative w-screen h-screen overflow-hidden">
-      <div className="flex flex-col p-8">
+      <div className="flex flex-col p-8 gap-4">
         <h1 className="font-alibaba text-3xl">
           تـــبریک! به{" "}
           <strong className="font-kalameh text-5xl">وصـــلیم</strong> خوش آمدید.
         </h1>
-        <div>
-          {/* <Image fill src={user.data[0].profile} alt="profile"/> */}
+        <div className="flex flex-col items-center gap-2 mt-10">
+          <Image
+            width={64}
+            height={64}
+            src={user.data[0].profile}
+            alt="profile"
+            className="bg-white rounded-full"
+          />
+          <span>{data.user.email.split("@")[0]}</span>
+        </div>
+        <div className="flex flex-col gap-8">
+          <p>شما همیشه میتوانید اطلاعات پروفایل خود را به روز رسانی کنید</p>
+          <Button path={"/home"} className="bg-blue text-white">
+            تو وصلیم چه خبره؟
+          </Button>
         </div>
       </div>
       <SignupCircle />
