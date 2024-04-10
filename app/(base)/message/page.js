3e5c9038fa84ef209459d19c3bb81b5dd1b9537b
moeinmac <moein.mac@gmail.com>
2024-04-10 16:54:46 +0430

    back to back!

--- app/(base)/message/page.js
+++ app/(base)/message/page.js
@@ -1,23 +1,4 @@
-"use client"
-
-import { createClient } from "@/lib/supabase/client";
-
 const message = () => {
-  const supabase = createClient();
-  const roomOne = supabase.channel("room_01");
-
-  roomOne
-    .on("presence", { event: "sync" }, () => {
-      const newState = roomOne.presenceState();
-      console.log("sync", newState);
-    })
-    .on("presence", { event: "join" }, ({ key, newPresences }) => {
-      console.log("join", key, newPresences);
-    })
-    .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
-      console.log("leave", key, leftPresences);
-    })
-    .subscribe();
   return <h1>message</h1>;
 };
 
--- app/(base)/pen/new/page.js
+++ app/(base)/pen/new/page.js
@@ -1,24 +1,4 @@
-"use client";
-
-import { createClient } from "@/lib/supabase/client";
-
 const newPen = () => {
-  const supabase = createClient();
-  const roomOne = supabase.channel("room_01");
-
-  const userStatus = {
-    user: "user-1",
-    online_at: new Date().toISOString(),
-  };
-
-  roomOne.subscribe(async (status) => {
-    if (status !== "SUBSCRIBED") {
-      return;
-    }
-
-    const presenceTrackStatus = await roomOne.track(userStatus);
-    console.log(presenceTrackStatus);
-  });
   return <h1>new Pen</h1>;
 };
 
