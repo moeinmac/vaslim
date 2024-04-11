6818f00e0d31d6dcde0956a0cc0ed564903da10e
Moein.MAC <moein.mac@gmail.com>
2024-04-11 18:56:57 +0330

    new pen header

--- app/(base)/pen/new/page.js
+++ app/(base)/pen/new/page.js
@@ -1,5 +1,7 @@
 const newPen = () => {
-  return <h1>new Pen</h1>;
+  return <header className="px-6 py-4">
+    <h1 className="font-kalameh text-5xl">نـوشتن قـــلم جدید</h1>
+  </header>;
 };
 
 export default newPen;
