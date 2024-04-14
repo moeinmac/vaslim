d7b444f34ed154bb8ee8d5c9c86eef089bbb24d5
Moein.MAC <moein.mac@gmail.com>
2024-04-14 11:06:18 +0330

    move signin function to seprate file

--- components/Auth/LoginForm.js
+++ components/Auth/LoginForm.js
@@ -1,29 +1,7 @@
-import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
+import { loginHandler } from "@/lib/loginHandler";
 import SubmitButton from "./SubmitButton";
 
 const LoginForm = ({ message }) => {
-  const signIn = async (formData) => {
-    "use server";
-    const supabase = createClient();
-
-    const username = formData.get("username");
-    const password = formData.get("password");
-    // if uesr's email don't start with gmail it crashes {#fix_later}
-    const email = username.includes("@") ? username : `${username}@gmail.com`;
-
-    const { error } = await supabase.auth.signInWithPassword({
-      email,
-      password,
-    });
-
-    if (error) {
-      return redirect("/auth?message=authentication-failed");
-    }
-
-    return redirect("/home");
-  };
-
   return (
     <form className="w-full flex flex-col gap-6 text-white">
       <label htmlFor="username" className="text-sm font-alibaba">
@@ -50,7 +28,7 @@ const LoginForm = ({ message }) => {
       </label>
 
       <SubmitButton
-        formAction={signIn}
+        formAction={loginHandler}
         pendingText="در حال ورود..."
         className="bg-orange text-black text-4xl font-kalameh rounded-xl px-8 py-4 "
       >
