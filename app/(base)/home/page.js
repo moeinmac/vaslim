3d1dbbd0c40fc2ed642fc59ab4f4c2a0f22b49c2
Moein.MAC <moein.mac@gmail.com>
2024-04-16 14:53:48 +0330

    improve some shit

--- app/(base)/home/page.js
+++ app/(base)/home/page.js
@@ -6,22 +6,14 @@ import SwitchHomePen from "@/components/pen/SwitchHomePen";
 import { FAKEDOTDATA } from "@/lib/FAKEDOTDATA";
 import { createClient } from "@/lib/supabase/server";
 
-import { convertedPenItems, getUsersByPrimary } from "@/lib/getUsersByPrimary";
-
-export const createExplorePen = async (vasl, myid) => {
-  const supabase = createClient();
-  const vaslUsers = await getUsersByPrimary(vasl, false, ["id"]);
-  const notWantedIDs = vaslUsers.map((user) => user.id);
-  notWantedIDs.push(myid);
-  const { data } = await supabase.from("pen").select().not("author", "in", `(${notWantedIDs})`);
-  return await convertedPenItems(data);
-};
-
 const Home = async ({ searchParams }) => {
   const supabase = createClient();
   const myAuth = await supabase.auth.getUser();
-  const { data } = await supabase.from("user").select().eq("id", myAuth.data.user.id);
-  const explorePenData = await createExplorePen(data[0].vasl, data[0].id);
+  const { data } = await supabase
+    .from("user")
+    .select("username,profile,vasl")
+    .eq("id", myAuth.data.user.id);
+
   return (
     <>
       <header className="flex justify-between items-center px-6 py-4">
@@ -34,8 +26,10 @@ const Home = async ({ searchParams }) => {
         isBlur={data[0].vasl.length > 1 ? true : false}
       />
       <SwitchHomePen params={searchParams} username={data[0].username} />
-      {!searchParams.fayre && <HomePen vasl={data[0].vasl} myUsername={data[0].username} />}
-      {searchParams.fayre && <ExpolorePen myUsername={data[0].username} penData={explorePenData} />}
+      {!searchParams.explore && <HomePen vasl={data[0].vasl} myUsername={data[0].username} />}
+      {searchParams.explore && (
+        <ExpolorePen myUsername={data[0].username} vasl={data[0].vasl} id={myAuth.data.user.id} />
+      )}
     </>
   );
 };
--- components/pen/ExpolorePen.js
+++ components/pen/ExpolorePen.js
@@ -1,10 +1,22 @@
 import HomePenItem from "./HomePenItem";
+import { convertedPenItems, getUsersByPrimary } from "@/lib/getUsersByPrimary";
+import { createClient } from "@/lib/supabase/server";
 
-const ExpolorePen = ({penData , myUsername}) => {
 
+export const createExplorePen = async (vasl, myid) => {
+  const supabase = createClient();
+  const vaslUsers = await getUsersByPrimary(vasl, false, ["id"]);
+  const notWantedIDs = vaslUsers.map((user) => user.id);
+  notWantedIDs.push(myid);
+  const { data } = await supabase.from("pen").select().not("author", "in", `(${notWantedIDs})`);
+  return await convertedPenItems(data);
+};
+
+const ExpolorePen =async ({vasl, id , myUsername}) => {
+  const explorePenData = await createExplorePen(vasl, id);
   return (
     <div className="flex flex-col gap-4 px-6">
-      {penData.map((pen) => (
+      {explorePenData.map((pen) => (
         <HomePenItem pen={pen} myUsername={myUsername} key={pen.id} />
       ))}
     </div>
