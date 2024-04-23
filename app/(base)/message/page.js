5673ba74faa3e0eb11f535869efdc103a2c6b380
Moein.MAC <moein.mac@gmail.com>
2024-04-23 12:26:47 +0330

    check if it about mobile tabbar?

--- app/(base)/message/page.js
+++ app/(base)/message/page.js
@@ -1,4 +1,4 @@
-// import Image from "next/image";
+import Image from "next/image";
 
 
 const message = () => {
@@ -6,13 +6,11 @@ const message = () => {
 
   return (
     <>
-      <div></div>
-
-      {/* <header className="px-6 py-4 flex flex-col gap-2">
+      <header className="px-6 py-4 flex flex-col gap-2">
         <h1 className="font-kalameh text-5xl">ارسال پیام در دست توسعه میباشد .</h1>
         <p className="font-alibaba text-2xl">منتظر آپــدیت های بــعدی باشید.</p>
       </header>
-      <Image  src={"/message.svg"} className="px-6" alt="پیام" fill/> */}
+      <Image  src={"/message.svg"} className="px-6" alt="پیام" fill/>
     </>
   );
 };
