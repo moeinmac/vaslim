ba18bdca3a694212b6237773b5c5a7824b780a77
Moein.MAC <moein.mac@gmail.com>
2024-04-27 11:34:57 +0330

    create expolre pen

--- components/pen/ExpolorePen.js
+++ components/pen/ExpolorePen.js
@@ -1,22 +1,11 @@
+import { createExplorePen } from "@/lib/pen/createExplorePen";
 import HomePenItem from "./HomePenItem";
-import { convertedPenItems, getUsersByPrimary } from "@/lib/getUsersByPrimary";
-import { createClient } from "@/lib/supabase/server";
 
-
-export const createExplorePen = async (vasl, myid) => {
-  const supabase = createClient();
-  const vaslUsers = await getUsersByPrimary(vasl, false, ["id"]);
-  const notWantedIDs = vaslUsers.map((user) => user.id);
-  notWantedIDs.push(myid);
-  const { data } = await supabase.from("pen").select().not("author", "in", `(${notWantedIDs})`);
-  return await convertedPenItems(data);
-};
-
-const ExpolorePen =async ({vasl, id , myUsername}) => {
-  const explorePenData = await createExplorePen(vasl, id);
+const ExpolorePen = async ({ vasl, id, myUsername }) => {
+  const allData = await createExplorePen(vasl, id);
   return (
     <div className="flex flex-col gap-4 px-6">
-      {explorePenData.map((pen) => (
+      {allData.map((pen) => (
         <HomePenItem pen={pen} myUsername={myUsername} key={pen.id} />
       ))}
     </div>
