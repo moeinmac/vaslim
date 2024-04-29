9d37fa1c710773c20842c3edf5a39d08104f3a4c
Moein.MAC <moein.mac@gmail.com>
2024-04-29 19:17:07 +0330

    test online state

--- components/message/MessageCard.js
+++ components/message/MessageCard.js
@@ -1,7 +1,5 @@
 "use client";
 
-import { createClient } from "@/lib/supabase/client";
-
 import { useRef, useState } from "react";
 import MessageHeader from "./MessageHeader";
 import MessageList from "./MessageList";
@@ -9,7 +7,9 @@ import NewMessage from "./NewMessage";
 
 const MessageCard = ({ userdata, myid, id, created_at, userid }) => {
   const scrollRef = useRef();
-  const supabase = createClient();
+  const [online, setOnline] = useState(false);
+  console.log(online);
+  const onlineHandler = (value) => setOnline(value);
 
   const scrolToBottom = () => {
     if (scrollRef.current) {
@@ -21,44 +21,24 @@ const MessageCard = ({ userdata, myid, id, created_at, userid }) => {
     }
   };
 
-  const [online , setOnline] = useState()
-
-  const roomOne = supabase.channel(`room-${id}`);
-
-  const userStatus = {
-    user: myid,
-    online_at: new Date().toISOString(),
-  };
-
-  roomOne
-    .on("presence", { event: "join" }, ({ key, newPresences }) => {
-      console.log({ userid: userid, new: newPresences[0].user });
-      if (newPresences[0].user && userid && newPresences[0].user === userid) {
-        setOnline(true)
-      }
-    })
-    .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
-      console.log("leave", key, leftPresences);
-    })
-    .subscribe(async (status) => {
-      if (status !== "SUBSCRIBED") {
-        return;
-      }
-      await roomOne.track(userStatus);
-    });
-
-    console.log(online);
-
   return (
     <div
       ref={scrollRef}
       className="noscroll mt-[6rem] flex flex-col h-[80vh] overflow-y-auto justify-between"
     >
-      <MessageHeader data={userdata} userid={userid} myid={myid} />
+      <MessageHeader
+        data={userdata}
+        userid={userid}
+        myid={myid}
+        onlineHandler={onlineHandler}
+        online={online}
+      />
+
       <div className="font-alibaba inline text-sm text-center py-6 text-zinc-400 ">
         این مکالمه در تاریخ {new Date(created_at).toLocaleString("fa-IR", { dateStyle: "medium" })}{" "}
         ایجاد شد.
       </div>
+
       <MessageList myid={myid} id={id} scrolToBottom={scrolToBottom} />
       <NewMessage myid={myid} id={id} />
     </div>
--- components/message/MessageHeader.js
+++ components/message/MessageHeader.js
@@ -1,20 +1,46 @@
 import BackButton from "../user/BackButton";
 import Profile from "../user/Profile";
+import { createClient } from "@/lib/supabase/client";
 
-const MessageHeader = ({ data, id, userid, myid }) => {
+const MessageHeader = ({ data, id, userid, myid, onlineHandler,online }) => {
+  const supabase = createClient();
 
+  const messageChannel = supabase.channel(`room-${id}-online`);
 
-
+  messageChannel
+    .on("presence", { event: "join" }, ({ key, newPresences }) => {
+      if (newPresences[0].user && userid && newPresences[0].user === userid) {
+        onlineHandler(true);
+      }
+    })
+    .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
+      if (leftPresences[0].user && userid && leftPresences[0].user === userid) {
+        onlineHandler(false);
+      }
+    })
+    .subscribe(async (status) => {
+      if (status !== "SUBSCRIBED") {
+        return;
+      }
+      await messageChannel.track({
+        user: myid,
+      });
+    });
 
   return (
     <header className="fixed top-0 bg-black w-full px-6 py-4 flex items-center justify-between border-b-2 border-zinc-900">
-      <Profile
-        profile={data.profile}
-        fullname={data.fullname}
-        username={data.username}
-        isVerified={data.isVerified}
-        small={true}
-      />
+      <div className="relative">
+        <Profile
+          profile={data.profile}
+          fullname={data.fullname}
+          username={data.username}
+          isVerified={data.isVerified}
+          small={true}
+        />
+        {online && (
+          <div className="absolute -bottom-2 -right-2 bg-green-500 w-5 h-5 rounded-full"></div>
+        )}
+      </div>
       <BackButton className={"text-4xl"} />
     </header>
   );
