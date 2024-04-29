28c1d79b95d3fd10116cd5507d8f548ffa1d3a6e
Moein.MAC <moein.mac@gmail.com>
2024-04-29 15:45:23 +0330

    test online

--- components/message/MessageCard.js
+++ components/message/MessageCard.js
@@ -1,12 +1,15 @@
 "use client";
 
-import { useRef } from "react";
+import { createClient } from "@/lib/supabase/client";
+
+import { useRef, useState } from "react";
 import MessageHeader from "./MessageHeader";
 import MessageList from "./MessageList";
 import NewMessage from "./NewMessage";
 
 const MessageCard = ({ userdata, myid, id, created_at, userid }) => {
   const scrollRef = useRef();
+  const supabase = createClient();
 
   const scrolToBottom = () => {
     if (scrollRef.current) {
@@ -18,12 +21,40 @@ const MessageCard = ({ userdata, myid, id, created_at, userid }) => {
     }
   };
 
+  const [online , setOnline] = useState()
+
+  const roomOne = supabase.channel(`room-${id}`);
+
+  const userStatus = {
+    user: myid,
+    online_at: new Date().toISOString(),
+  };
+
+  roomOne
+    .on("presence", { event: "join" }, ({ key, newPresences }) => {
+      console.log({ userid: userid, new: newPresences[0].user });
+      if (newPresences[0].user && userid && newPresences[0].user === userid) {
+        setOnline(true)
+      }
+    })
+    .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
+      console.log("leave", key, leftPresences);
+    })
+    .subscribe(async (status) => {
+      if (status !== "SUBSCRIBED") {
+        return;
+      }
+      await roomOne.track(userStatus);
+    });
+
+    console.log(online);
+
   return (
     <div
       ref={scrollRef}
       className="noscroll mt-[6rem] flex flex-col h-[80vh] overflow-y-auto justify-between"
     >
-      <MessageHeader data={userdata} userid={userid} myid={myid}/>
+      <MessageHeader data={userdata} userid={userid} myid={myid} />
       <div className="font-alibaba inline text-sm text-center py-6 text-zinc-400 ">
         این مکالمه در تاریخ {new Date(created_at).toLocaleString("fa-IR", { dateStyle: "medium" })}{" "}
         ایجاد شد.
--- components/message/MessageHeader.js
+++ components/message/MessageHeader.js
@@ -1,36 +1,10 @@
-import { createClient } from "@/lib/supabase/client";
 import BackButton from "../user/BackButton";
 import Profile from "../user/Profile";
 
-const MessageHeader = ({ data, id, userid , myid }) => {
-  const supabase = createClient();
+const MessageHeader = ({ data, id, userid, myid }) => {
 
-  const roomOne = supabase.channel("room-562938");
 
-  const userStatus = {
-    user: myid,
-    online_at: new Date().toISOString(),
-  };
 
-  roomOne
-    .on("presence", { event: "sync" }, () => {
-      const newState = roomOne.presenceState();
-      console.log("sync", newState);
-    })
-    .on("presence", { event: "join" }, ({ key, newPresences }) => {
-      console.log("join", key, newPresences);
-    })
-    .on("presence", { event: "leave" }, ({ key, leftPresences }) => {
-      console.log("leave", key, leftPresences);
-    })
-    .subscribe(async (status) => {
-      if (status !== "SUBSCRIBED") {
-        return;
-      }
-
-      const presenceTrackStatus = await roomOne.track(userStatus);
-      console.log(presenceTrackStatus);
-    });
 
   return (
     <header className="fixed top-0 bg-black w-full px-6 py-4 flex items-center justify-between border-b-2 border-zinc-900">
