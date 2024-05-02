daa2a38c637652ed26385474e75db1f16892b2dc
Moein.MAC <moein.mac@gmail.com>
2024-05-02 11:29:36 +0330

    add some loading to fetch pages

---
+++ app/(base)/user/vasl/loading.js
@@ -0,0 +1,17 @@
+const loading = () => {
+  return (
+    <main>
+      <div className="flex justify-center items-center flex-col gap-4 h-[85vh] px-4">
+        <div className="loader"></div>
+        <h1 className="font-kalameh text-3xl text-center">
+          تحمل کن عزیز دل شکسته <br />
+          تحمل کن به پای شمع خاموش
+        </h1>
+      </div>
+    </main>
+  );
+};
+
+
+
+export default loading;
